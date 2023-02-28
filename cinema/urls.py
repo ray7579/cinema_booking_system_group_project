67b099c7bfc1be92da343fa9d3f3c3f99ee4e253
r3-puddy <ryan2.puddy@live.uwe.ac.uk>
2023-02-28 15:14:49 +0000

    Film Add, Update, Delete. Cinema Manager

--- cinema/urls.py
+++ cinema/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path("movies/", views.list_movies, name = "list_movies"),
     path("show_movie/<movie_id>", views.show_movie, name = "show-movie"),
     path("confirmation/<movie_id>",views.confirm_movie,name = "confirm_movie"),
+    path('renfilmhome/addfilm/', views.addfilm, name="addfilm"),
+    path('renfilmhome/updatefilm/<movie_id>', views.updatefilm, name="updatefilm"),
+    path('renfilmhome/delete/<film_id>', views.delete, name="delete"),
+    path('renfilmhome/', views.renfilmhome, name = "renfilmhome"),
 
 ]
 
