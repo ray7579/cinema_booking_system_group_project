8fc16e9c4df95d32762f01988b8870fcdb709a6c
r3-puddy <ryan2.puddy@live.uwe.ac.uk>
2023-02-28 15:55:27 +0000

    Added film duration to the data (in specification)

--- cinema/forms.py
+++ cinema/forms.py
@@ -4,4 +4,4 @@ from .models import Movie
 class filmForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ['name', 'description', 'age_rating', 'image']
+        fields = ['name', 'description', 'age_rating', 'duration','image']
